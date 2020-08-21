117d5fb86068d9dbf3d3de833aa277c5e4f90c66
Hanope <heemanghan@gmail.com>
2020-08-21 21:53:14 +0900

    init

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/fast")
+    public String helloFast() {
+        return "fast";
+    }
+
+    @GetMapping("/slow")
+    public String helloSlow() throws InterruptedException {
+        Thread.sleep(5000);
+        return "slow";
+    }
+}
